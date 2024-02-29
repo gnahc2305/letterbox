3a04455ab8a450b59ce67456a44b5c311dc11a9f
gnahc2305 <achangmurillo@gmail.com>
2024-02-29 17:49:03 -0500

    setup next auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,28 +8,28 @@ import { useState } from "react";
 export default function Home() {
   const [media, setMedia] = useState("");
 
-  async function test(query: string) {
-    const options = {
-      method: "GET",
-      url: "https://imdb8.p.rapidapi.com/title/find",
-      params: {
-        q: query,
-      },
-      headers: {
-        "X-RapidAPI-Key": process.env.RAPID_API_KEY,
-        "X-RapidAPI-Host": process.env.RAPID_API_HOST,
-      },
-    };
+  // async function test(query: string) {
+  //   const options = {
+  //     method: "GET",
+  //     url: "https://imdb8.p.rapidapi.com/title/find",
+  //     params: {
+  //       q: query,
+  //     },
+  //     headers: {
+  //       "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+  //       "X-RapidAPI-Host": process.env.RAPID_API_HOST,
+  //     },
+  //   };
 
-    try {
-      const response = await axios.request(options);
-      console.log(response.data.results[0]);
-    } catch (error) {
-      console.error(error);
-    }
-  }
+  //   try {
+  //     const response = await axios.request(options);
+  //     console.log(response.data.results[0]);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
 
-  test('succecion');
+  // test('succecion');
 
   return (
     <div>
